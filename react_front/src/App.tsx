cf3926a5eadc719b655c0de32ee834d92117fa85
Mathis LAMBERT <mathis.lambert27@gmail.com>
2024-08-17 15:02:35 +0200

    [+] update front

--- react_front/src/App.tsx
+++ react_front/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+          <p>API URL : {import.meta.env.VITE_API_URL}</p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
